d28f569afbd5a05c7342cb382aad9d31dafd21c0
DinhSonHai <sdinhhai@gmail.com>
2020-10-06 15:50:49 +0700

    Add style

--- tetris.js
+++ tetris.js
@@ -15,6 +15,23 @@ function collide(arena, player) {
   return false;
 }
 
+function arenaSweap() {
+  let rowCount = 1;
+  outer: for (let y = arena.length - 1; y > 0; y--) {
+    for (let x = 0; x < arena[y].length; x++) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+    const x = arena.splice(y, 1);
+    const row = x[0].fill(0);
+    arena.unshift(row);
+    y++;
+    player.score += rowCount * 10;
+    rowCount *= 2;
+  }
+}
+
 function createMatrix(w, h) {
   const matrix = [];
   while (h--) {
@@ -33,38 +50,38 @@ function createBlock(type) {
       ];
     case 'O':
       return [
-        [1, 1],
-        [1, 1],
+        [2, 2],
+        [2, 2],
       ];
     case 'L':
       return [
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 1],
+        [0, 3, 0],
+        [0, 3, 0],
+        [0, 3, 3],
       ];
     case 'J':
       return [
-        [0, 1, 0],
-        [0, 1, 0],
-        [1, 1, 0],
+        [0, 4, 0],
+        [0, 4, 0],
+        [4, 4, 0],
       ];
     case 'I':
       return [
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
       ];
     case 'S':
       return [
-        [0, 1, 1],
-        [1, 1, 0],
+        [0, 6, 6],
+        [6, 6, 0],
         [0, 0, 0],
       ];
     case 'Z':
       return [
-        [1, 1, 0],
-        [0, 1, 1],
+        [7, 7, 0],
+        [0, 7, 7],
         [0, 0, 0],
       ];
   }
@@ -75,6 +92,11 @@ function playerReset() {
   player.matrix = createBlock(blocks[(blocks.length * Math.random()) | 0]);
   player.pos.y = 0;
   player.pos.x = ((arena[0].length / 2) | 0) - ((player.matrix.length / 2) | 0);
+  if (collide(arena, player)) {
+    arena.forEach((row) => row.fill(0));
+    player.score = 0;
+    updateScore();
+  }
 }
 
 function draw() {
@@ -89,7 +111,7 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
-        context.fillStyle = 'red';
+        context.fillStyle = colors[value];
         context.fillRect(x + offset.x, y + offset.y, 1, 1);
       }
     });
@@ -119,6 +141,8 @@ function playerDrop() {
     player.pos.y--;
     merge(arena, player);
     playerReset();
+    arenaSweap();
+    updateScore();
   }
   dropCounter = 0;
 }
@@ -167,13 +191,29 @@ function update(time = 0) {
   requestAnimationFrame(update);
 }
 
+const colors = [
+  null,
+  'red',
+  'blue',
+  'violet',
+  'green',
+  'purple',
+  'orange',
+  'pink',
+];
+
 const arena = createMatrix(12, 20);
 console.table(arena);
 
 const player = {
-  pos: { x: 5, y: 5 },
-  matrix: createBlock('T'),
+  pos: { x: 0, y: 0 },
+  matrix: null,
+  score: 0,
 };
+
+function updateScore() {
+  document.getElementById('scrore').innerText = player.score;
+}
 
 document.addEventListener('keydown', (e) => {
   if (e.keyCode === 37) {
@@ -186,5 +226,6 @@ document.addEventListener('keydown', (e) => {
     playerRotate(-1);
   }
 });
-
+playerReset();
+updateScore();
 update();
